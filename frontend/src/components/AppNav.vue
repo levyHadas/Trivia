9db276f3d9e18be38d6301a872af9287f2d548c3
Sarel <Sarelk@gmail.com>
2019-03-20 14:22:42 +0200

    hompage layout

---
+++ frontend/src/components/AppNav.vue
@@ -0,0 +1,22 @@
+<template>
+  <nav>
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link> |
+      <router-link to="/signup">Sign Up</router-link> |
+      <router-link to="/login">Login</router-link>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: 'Navbar',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+
+</style>
--- frontend/src/router.js
+++ frontend/src/router.js
@@ -1,6 +1,9 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from './views/Home.vue'
+import Game from './views/GameScreen.vue'
+import SignUp from './views/SignUp.vue'
+import Login from './views/login.vue'
 
 Vue.use(Router)
 
@@ -10,6 +13,21 @@ export default new Router({
       path: '/',
       name: 'home',
       component: Home
+    },
+    {
+      path: '/play',
+      name: 'game',
+      component: Game
+    },
+    {
+      path: '/signup',
+      name: 'SignUp',
+      component: SignUp
+    },
+    {
+      path: '/login',
+      name: 'login',
+      component: Login
     },
 
   ]
