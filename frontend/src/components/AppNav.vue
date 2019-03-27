0639cdcbec25921847e1044be31c7f7f2c0ddd2c
Ido Ayal <idodidodi@gmail.com>
2019-03-27 15:00:22 +0200

    Add condition to create question

--- frontend/src/components/AppNav.vue
+++ frontend/src/components/AppNav.vue
@@ -1,47 +1,57 @@
 <template>
   <nav>
-      <router-link to="/">Home</router-link>
-      <span> | </span> 
-      <router-link to="/about">About</router-link>
-      <span> | </span> 
-      <router-link to="/signup">Sign Up</router-link>
-      <span> | </span> 
-      <router-link to="/login" v-if="!realUser">Login</router-link>
-      <span v-if="!realUser"> | </span>
-      <router-link to="/quest/edit">Create Question</router-link>
-      <span v-if="realUser"> | </span>
-      <router-link to="/logout" v-if="realUser">Logout</router-link>
-      <span v-if="realUser"> | </span>
-      <img :src="loggedUser.img" width="50px" v-if="realUser">
+    <router-link to="/">Home</router-link>
+    <span>|</span>
+    <router-link to="/about">About</router-link>
+    <span>|</span>
+    <router-link to="/signup">Sign Up</router-link>
+    <span>|</span>
+    <router-link to="/login" v-if="!realUser">Login</router-link>
+    <span v-if="!realUser">|</span>
+    <button @click="navToCreateQuest" class="nav-btn">Create Question</button>
+    <span>|</span>
+    <router-link to="/logout" v-if="realUser">Logout</router-link>
+    <span v-if="realUser">|</span>
   </nav>
 </template>
 
 <script>
 export default {
-  name: 'Navbar',
+  name: "Navbar",
   props: {
     msg: String
   },
-
+  methods: {
+    navToCreateQuest() {
+      if (this.realUser) this.$router.push("/quest/edit");
+      else this.$message("Please login or sign up to create question");
+    }
+  },
   computed: {
-    loggedUser(){
-      return this.$store.getters.currUser
+    loggedUser() {
+      return this.$store.getters.currUser;
     },
     realUser() {
-      const user = this.$store.getters.currUser
-      if (!user._id) return false
-      if(user._id.includes('guest')) return false
-      return true
-
-
+      const user = this.$store.getters.currUser;
+      if (!user._id) return false;
+      if (user._id.includes("guest")) return false;
+      return true;
     }
-
   }
-
-}
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
-
+.nav-btn {
+  background: none;
+  border: none;
+  padding: 0;
+  font: inherit;
+  cursor: pointer;
+  outline: inherit;
+  font-weight: bold;
+  color: #2c3e50;
+  text-decoration: underline;
+}
 </style>
