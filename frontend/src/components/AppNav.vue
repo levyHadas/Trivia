6413a5d34b46656e64e575710999ad5c234ecac5
Sarel <Sarelk@gmail.com>
2019-03-27 14:40:53 +0200

    homePage

--- frontend/src/components/AppNav.vue
+++ frontend/src/components/AppNav.vue
@@ -1,48 +1,46 @@
 <template>
   <nav>
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link> |
-      <router-link to="/signup">Sign Up</router-link> |
-      <router-link to="/login" v-if="!realUser">Login |</router-link> 
-      <router-link to="/logout" v-if="realUser">Logout</router-link>
-      <p v-if="realUser">Hello: {{loggedUser.username}}</p>
-      <img :src="loggedUser.img" width="50px">
+    <router-link to="/">Home</router-link>|
+    <router-link to="/about">About</router-link>|
+    <router-link to="/signup">Sign Up</router-link>|
+    <router-link to="/login" v-if="!realUser">Login |</router-link>
+    <router-link to="/quest/edit" v-if="realUser">Create Question</router-link>
+    <span v-if="realUser">|</span>
+    <router-link to="/logout" v-if="realUser">Logout</router-link>
+    <p v-if="realUser">Hello: {{loggedUser.username}}</p>
+    <img :src="loggedUser.img" width="50px">
   </nav>
 </template>
 
 <script>
 export default {
-  name: 'Navbar',
+  name: "Navbar",
   props: {
     msg: String
   },
 
   computed: {
-    loggedUser(){
-      return this.$store.getters.currUser
+    loggedUser() {
+      return this.$store.getters.currUser;
     },
     realUser() {
-      const user = this.$store.getters.currUser.username
-      if (this.$store.getters.currUser.username) return true
-      return false
-    //   if (!user) return false
-    //   if (user._id.includes('guest')) {
-    //     console.log(user._id.includes('guest'), 'real user?')
-    //     return false
-    //   }
-    //   return true
-
+      const user = this.$store.getters.currUser.username;
+      if (this.$store.getters.currUser.username) return true;
+      return false;
+      //   if (!user) return false
+      //   if (user._id.includes('guest')) {
+      //     console.log(user._id.includes('guest'), 'real user?')
+      //     return false
+      //   }
+      //   return true
     }
-
   }
-
-}
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
-  p {
-    text-transform: uppercase;
-  }
-
+p {
+  text-transform: uppercase;
+}
 </style>
